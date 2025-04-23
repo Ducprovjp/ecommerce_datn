c7ffe2e3bc4ea7133e9f0ffc406df9adb5d4b529
Ducprovjp <ducprovjp2612@gmail.com>
2025-04-23 16:15:07 +0700

    connect fe be

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,9 @@
-export const server = "http://localhost:8000/api/v2";
+//development
+// export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+// export const backend_url = "http://localhost:8000/";
+
+//production
+export const server = "https://ecommerce-datn.onrender.com/api/v2";
+
+export const backend_url = "https://ecommerce-datn.onrender.com/";
