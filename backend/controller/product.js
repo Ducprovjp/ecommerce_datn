90bd66cc8453859982cfa3e5f77de6d5554f6861
Ducprovjp <ducprovjp2612@gmail.com>
2025-04-25 15:48:51 +0700

    upload image cloudinary

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -19,21 +19,21 @@ router.post(
       const shop = await Shop.findById(shopId);
       if (!shop) {
         return next(new ErrorHandler("Shop Id is invalid!", 400));
-      } else {
-        const files = req.files;
-        const imageUrls = files.map((file) => `${file.filename}`);
-
-        const productData = req.body;
-        productData.images = imageUrls;
-        productData.shop = shop;
-
-        const product = await Product.create(productData);
-
-        res.status(201).json({
-          success: true,
-          product,
-        });
       }
+
+      const files = req.files;
+      const imageUrls = files.map((file) => file.path); // dùng .path để lấy URL trên cloudinary
+
+      const productData = req.body;
+      productData.images = imageUrls;
+      productData.shop = shop;
+
+      const product = await Product.create(productData);
+
+      res.status(201).json({
+        success: true,
+        product,
+      });
     } catch (error) {
       return next(new ErrorHandler(error, 400));
     }
