563b5f7450b9f3c2ae2a12859699027a1ec5d2ca
Ducprovjp <ducprovjp2612@gmail.com>
2025-04-26 16:25:06 +0700

    BE render

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,9 +1,9 @@
 //development
-export const server = "http://localhost:8000/api/v2";
+// export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+// export const backend_url = "http://localhost:8000/";
 
 //production
-// export const server = "https://ecommerce-datn.onrender.com/api/v2";
+export const server = "https://ecommerce-datn.onrender.com/api/v2";
 
-// export const backend_url = "https://ecommerce-datn.onrender.com/";
+export const backend_url = "https://ecommerce-datn.onrender.com/";
